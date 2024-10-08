1cb8bcc5dcc356ce40f778ee5a7e08cb95790979
anthonypham26 <anthonypham.c22@gmail.com>
2024-10-08 12:07:10 -0500

    Added Examples

--- PartA.py
+++ PartA.py
@@ -1,4 +1,5 @@
-for i in range(10):
-    print("Can I get a penny on pump 4")
-    print("I HAVE NOTHING")
-    
+float_list = float[-16.915, 0.279, -35.794, 33.724, -43.779, -46.629, 14.696, 85.361, 38.644, -60.172]
+
+sting_list = str['Olivia', 'Liam', 'Lepookie', 'Noah', 'Ava', 'Ethan', 'Bron', 'Mason', 'Isabella', 'James']
+
+int_list = int[23, 6, 0, 77, 99, 41, 31, 89, 12, 14]
