94c28e1ce9d12fa6af6234b4e1413e0f5144c153
anthonypham26 <anthonypham.c22@gmail.com>
2024-10-08 22:35:18 -0500

    Part A Done

--- PartA.py
+++ PartA.py
@@ -6,12 +6,10 @@ def floats_high_to_low(float_list):
     heapA = BinaryHeap(max_compare)
     heapA.build_heap(float_list)
     heapA.display()
-    for nodes in range(heapA.size()):
+    while heapA.size() > 0:
         sorted_floats.append(heapA.pop())
     return sorted_floats
 
-# list_a = [-16.915, 0.279, -35.794, 33.724, -43.779, -46.629, 14.696, 85.361, 38.644, -60.172]
-# print(floats_high_to_low(list_a))
 
 #Problem B:
 """Comparison function for a ascii min heap that preserves alphabetical order."""
@@ -30,13 +28,32 @@ def sort_string_alpha(string_list):
     heapB = BinaryHeap(ascii_min_compare)
     heapB.build_heap(string_list)
     heapB.display()
-    for nodes in range(heapB.size()):
+    while heapB.size() > 0:
         sorted_names.append(heapB.pop())
     return sorted_names
 
-# string_list = ['Olivia', 'liam', 'Lepookie', 'Noah', 'ava', 'Ethan', 'Bron', 'Mason', 'isabella', 'James']
-# print(sort_string_alpha(string_list))
 
 #Problem C:
-int_list = [23, 61, 00, 77, 96, 45, 38, 89, 12, 14]
+def secondDigit_min_compare(a, b):
+    a = int(str(abs(a))[1])
+    b = int(str(abs(b))[1])
+    return a < b
 
+def secondDigit_low_to_high(int_list):
+    sorted_ints = []
+    heapC = BinaryHeap(secondDigit_min_compare)
+    heapC.build_heap(int_list)
+    heapC.display()
+    while heapC.size() > 0:
+        sorted_ints.append(heapC.pop())
+    return sorted_ints
+
+if __name__ == '__main__':
+    list_a = [-16.915, 0.279, -35.794, 33.724, -43.779, -46.629, 14.696, 85.361, 38.644, -60.172]
+    print(floats_high_to_low(list_a))
+
+    list_b = ['Olivia', 'liam', 'Lepookie', 'Noah', 'ava', 'Ethan', 'Bron', 'Mason', 'isabella', 'James']
+    print(sort_string_alpha(list_b))
+
+    list_c = [231, 613, 12, -277, 96, -145, 38, 898, 125, 14]
+    print(secondDigit_low_to_high(list_c))
